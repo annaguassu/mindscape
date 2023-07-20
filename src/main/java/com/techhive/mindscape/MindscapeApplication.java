3d71c53bb721f12e4c154015af8363077d8ac706
Ana Guassu <annaguassu@gmail.com>
2023-07-19 23:59:47 -0300

    primeiro commit

---
+++ src/main/java/com/techhive/mindscape/MindscapeApplication.java
@@ -0,0 +1,13 @@
+package com.techhive.mindscape;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindscapeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindscapeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techhive/mindscape/MindscapeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techhive.mindscape;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindscapeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
